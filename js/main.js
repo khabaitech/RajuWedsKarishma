cb86b8aff6d5f3b87ac787824f29813e68e64ec5
Amisha-coder <60833377+Amisha-coder@users.noreply.github.com>
2021-11-05 19:57:10 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -303,7 +303,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("29 Nov 2021 00:06:00 GMT+01:00");			
+		var endTime = new Date("29 Nov 2021 00:07:00 GMT+01:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
